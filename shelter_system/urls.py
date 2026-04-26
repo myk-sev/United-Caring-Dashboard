354762dffd3580dd2eeac2e43b6891971f02894c
Mykhaylo Severinov <sev.myk@outlook.com>
2026-04-26 11:29:19 -0500

    Fix: the default url now loads mainscreen

--- shelter_system/urls.py
+++ shelter_system/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
+import mainscreen.views
+
 urlpatterns = [
+    path('', mainscreen.views.mainscreen),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('dashboard/', include('dashboard.urls')),
