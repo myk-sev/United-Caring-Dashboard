55f275fbd997632e72c6d69a3c10faf347a25fd2
Mykhaylo Severinov <sev.myk@outlook.com>
2026-04-26 11:44:48 -0500

    Fix: Homepage route set to / rather than /mainscreen. Redirect no longer needed.

--- shelter_system/urls.py
+++ shelter_system/urls.py
@@ -4,13 +4,12 @@ from django.urls import path, include
 import mainscreen.views
 
 urlpatterns = [
-    path('', mainscreen.views.HomepageRedirect),
+    path('', include('mainscreen.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('dashboard/', include('dashboard.urls')),
     path('reports/', include('reports.urls')),
     path('shelters/', include('shelters.urls')),
-    path('mainscreen/', include('mainscreen.urls')),
     path('white-flag/', include('whiteflag.urls')),
     path('', include('admin_panel.urls')),
 ]
